b31c1f63875ab1a3ecfafbb4a10fa1543098d4ba
Victor Socaci <Victor.Socaci@endava.com>
2026-01-08 16:39:23 +0200

    add files

---
+++ TravelJournalEnd/src/main/java/org/example/traveljournalend/TravelJournalEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.traveljournalend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelJournalEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelJournalEndApplication.class, args);
+    }
+
+}
---
+++ TravelJournalEnd/src/test/java/org/example/traveljournalend/TravelJournalEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.traveljournalend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelJournalEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
